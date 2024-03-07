06920381f29a7e25666512cef9b2b86436349755
Harison Okonkwo <92615528+Harison2016@users.noreply.github.com>
2024-03-07 16:26:04 -0500

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "harry-boto3-bucket" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "EKS-Hotstar/terraform.tfstate"
     region = "us-east-2"
   }
 }
