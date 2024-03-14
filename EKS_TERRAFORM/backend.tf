4fa4f87ae050f6367a02a43710de7b0171208893
Harison Okonkwo <92615528+Harison2016@users.noreply.github.com>
2024-03-14 10:12:36 -0400

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "harry-boto3-bucket" # Replace with your actual S3 bucket name
-    key    = "EKS-Hotstar/terraform.tfstate"
+    key    = "EKS-Youtube/terraform.tfstate"
     region = "us-east-2"
   }
 }
